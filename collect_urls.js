8aa12cf2d9217f19df43470319c7b5821ddbf8db
Jeff Hykin <jeffhykin@jeffs-imac.lan>
2019-08-13 16:47:45 -0500

    get url collection working again

--- collect_urls.js
+++ collect_urls.js
@@ -5,38 +5,74 @@ const cheerio = require("cheerio")
 let allUrls = new Set()
 let allNodes = new Set()
 let $
+let body = null
+
+let htmlSample = (node) => {
+    string = $(node).html() 
+    if (typeof string == 'string') {
+        string = string.trim()
+        string = string.replace(/\n/g, "\\n")
+        return `${string.substr(0,60)}`
+    }
+    return ""
+}
+let depth = ""
 let findUrls = (nodes, reset = true) => {
     if (reset) {
         allUrls = new Set()
         allNodes = new Set()
     }
-    nodes.each((i, each) => {
+    // $(nodes).each((i,each)=>{
+    for (let each of nodes) {
         each = $(each)
-        // get href
-        if (each instanceof Object && each.attr instanceof Function) {
-            let href = each.attr("href")
-            if (typeof href == "string") {
-                if (href.startsWith("/watch?")) {
-                    allUrls.add(href)
-                }
-            }
+
+        // if it hasn't been seen then evaluate it
+        if (!allNodes.has(each)) {
+            // check href
+            
         }
-        // if not seen before, and hash children
-        if (!allNodes.has(each) && each.each instanceof Function) {
-            // recurse
-            findUrls(each, false)
-        }
-        // add node to allNodes to prevent infinite looping
-        allNodes.add(each)
-    })
+    }
+    // }) 
+
     return allUrls
 }
 // make the request to youtube
 request("https://www.youtube.com", (error, response, html) => {
     if (!error && response.statusCode == 200) {
         $ = cheerio.load(html)
-
-        let body = $("body")
-        let urls = findUrls(body)
+        let allNodes = $('*')
+        // allow iterating on the children
+        allNodes.constructor.prototype[Symbol.iterator] = function () {
+            let index = -1
+            let all = []
+            while(this[++index] != undefined) {
+                all.push(this[index])
+            }
+            return {
+                next() {
+                    return {
+                        value: all.length && all.pop(),
+                        done: !all.length
+                    }
+                }
+            }
+        }
+        for (let each of allNodes) {
+            each = $(each)
+            if (each instanceof Object && each.attr instanceof Function) {
+                let href = each.attr("href")
+                if (typeof href == "string") {
+                    if (href.startsWith("/watch?")) {
+                        allUrls.add(href)
+                    }
+                }
+            }
+        }
+        let existingUrls = require("./urls.json")
+        for (let each of allUrls) {
+            console.log(`each is:`,each)
+            existingUrls[each] = true
+        }
+        fs.writeFileSync("./urls.json", JSON.stringify(existingUrls))
     }
 })
