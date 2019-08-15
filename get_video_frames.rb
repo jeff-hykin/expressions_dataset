f1fba45abe2e81e7e71c3fd6384d8442d6887538
Jeff Hykin <jeffhykin@jeffs-imac.lan>
2019-08-15 14:34:56 -0500

    get basic frames from a video

--- get_video_frames.rb
+++ get_video_frames.rb
@@ -42,8 +42,48 @@ end
 # this part of the code was derived from https://blog.francium.tech/take-screenshot-using-ruby-selenium-webdriver-b18802822075
 browser = BrowserPool.instance.get_browser
 browser.navigate.to 'https://www.youtube.com/watch?v=6005JSrES34'
-video_height  = browser.execute_script("return document.getElementsByClassName('video-stream html5-main-video')[0].clientHeight;")
-width  = browser.execute_script("return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);")
-height = browser.execute_script("return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
-browser.manage.window.resize_to([width+100, 1500].min, video_height * 1.2) #Restrict to maximum of 1500 x 3000
+
+# create a helper 
+wait_for_elements = ->(*args) do
+    elements = []
+    loop do
+        elements = browser.find_elements(*args)
+        if elements.size() > 0
+            if elements[0].displayed?() and elements[0].enabled?()
+                break
+            end
+        else
+            sleep 5
+        end
+    end
+    elements
+end
+
+# resize the page
+browser.manage.window.resize_to(width = 2000, height = (width/16.0)*9) # uses 16:9 aspect ratio
+
+# try to press the play button
+play_button = wait_for_elements[:class, "ytp-play-button"][0]
+play_button.click
+sleep 1
+
+# TODO: try to press the skip-advertisement button, and wait until the advertisement is done
+
+# make the video fullscreen
+browser.execute_script <<-HEREDOC
+    let videoElement = document.getElementsByClassName("video-stream")[0]
+    // move the video to the top
+    document.body.insertBefore(videoElement, document.body.childNodes[0])
+    // fix the video to the top
+    videoElement.style.position = "fixed"
+    // put the video on top of everything else
+    videoElement.style.zIndex = "999999999"
+    // resize it to fullscreen
+    videoElement.style.width = "#{width}px"
+    videoElement.style.height = "#{height}px"
+HEREDOC
+# wait for the page to load
+sleep 1
+
+# save a screenshot at that point
 browser.save_screenshot("sample.png")
