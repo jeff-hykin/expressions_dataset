5e55e123015c68bbda329ae97b7d304537959fde
Jeff Hykin <jeffhykin@jeffs-imac.lan>
2019-09-08 14:12:16 -0500

    _add Filesys tools and bounding boxes

---
+++ face_detection/script_gen_strange_face_boxes.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from os.path import join, dirname
+exec(Path(join(dirname(__file__),'./tools.py')).read_text())
+
+for each in FS.list_files(FS.dirname(__file__)+'/strange_face_detection'):
+    if FS.extname(each) == '.png':
+        *other_folders, strange_face_folder, filename, file_extension = FS.path_peices(each)
+        path = FS.join(*other_folders, "strange_faces_with_boxes", filename+file_extension)
+        Image(each).with_facial_bounding_boxes().save(path)
