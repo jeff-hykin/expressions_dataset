28d2c8ae64fdf941beb353e38da0e13ab627f4af
Jeff Hykin <jeffhykin@main-10-231-55-121.tamulink.tamu.edu>
2019-09-18 11:54:21 -0500

    _

--- detect_eyebrow_raise/main.py
+++ detect_eyebrow_raise/main.py
@@ -2,6 +2,10 @@ from pathlib import Path
 from os.path import join, dirname
 exec(Path(join(dirname(__file__),'..', 'face_detection', 'tools.py')).read_text())
 eyebrow_locations = []
+
+# TODO:
+    # scale it by the height of the face (top most of face vs bottom most)
+    # switch to using min of the eye-to-eyebrow distance
 for each_video_path in FS.list_files(join(dirname(__file__),"../clips.nosync/raised_eyebrows")):
     each_video = Video(each_video_path)
     # load the video and break it up into frames
