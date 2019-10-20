0c2b71e2b7ea5cc038236b123624903f50a35a48
Jeff Hykin <jeff.hykin@gmail.com>
2019-10-20 15:01:34 -0500

    add cross validation

--- face_detection/tools.py
+++ face_detection/tools.py
@@ -20,6 +20,38 @@ import cv2
 from ruamel.yaml import YAML
 yaml = YAML()
 
+
+def cross_validate(data, labels, train_and_validate_function, number_of_folds=6):
+    import numpy as np
+    """
+    data:
+        needs to have its first dimension (the len()) be the number of data points
+    labels:
+        needs to be the same length as data
+    train_and_validate_function:
+        needs to have 4 arguments, train_data, train_labels, test_data, and test_labels
+        it should return accuracy information as output
+    number_of_folds:
+        needs to be a number
+    """
+    # check number of folds
+    if (len(data) % number_of_folds):
+        raise Exception("The data needs to be divisible by the number of folds")
+    
+    results = []
+    batch_size = int(len(data) / number_of_folds)
+    for batch_number in range(number_of_folds):
+        print("\nOn fold:",batch_number+1)
+        start_index = batch_number * batch_size
+        end_index = (batch_number + 1) * batch_size
+        test_data = data[start_index:end_index]
+        test_labels = labels[start_index:end_index]
+        train_data   = np.concatenate((  data[0:start_index],   data[end_index:len(data)-1]))
+        train_labels = np.concatenate((labels[0:start_index], labels[end_index:len(data)-1]))
+        results.append(train_and_validate_function(train_data, train_labels, test_data, test_labels))
+    return results
+
+
 def ndarray_to_list(ndarray):
     if type(ndarray) != numpy.ndarray:
         return ndarray
