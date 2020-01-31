2e7db682a123ad372ac3fbe44ab39d4e7dd7a0c1
Jeff Hykin <jeff.hykin@gmail.com>
2020-01-31 12:05:35 -0600

    add bad video query

--- database_management/query.rb
+++ database_management/query.rb
@@ -2,6 +2,25 @@ require 'atk_toolbox'
 require_relative Info.paths["ruby_tools"] # the (path) inside info.yaml 
 
 
-# p Video['81YAIU3hzJ4'].metadata
+# take a look at the first several videos
+
+def list_nonhelpful_videos()
+    for each_video_id in Video.all_videos.keys[1...1000]
+        video = Video[each_video_id]
+        if not video["unavailable"]
+            # if facedata was collected
+            if video["facedata_version"] != nil && 
+                # but the video wasn't good
+                if not video["good_faces"]
+                    puts "no good faces: #{video.url}"
+                end
+            end
+        end
+    end
+end
+
+
+
+puts Video['8wOJw4VhmYs']
 # puts Video['K72HTHUwP54'].metadata.to_yaml
-puts Video['xnkCgOVBOsI'].all_faces_in_frames.to_yaml
+# puts Video['xnkCgOVBOsI'].all_faces_in_frames.to_yaml
