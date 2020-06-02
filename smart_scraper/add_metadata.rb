a66dcd694b3626e3b0a38d9fe6125adb83c2a74b
jeff-hykin <jeff.hykin@gmail.com>
2020-06-01 21:36:37 -0500

    WIP: updating the metadata collector

--- smart_scraper/add_metadata.rb
+++ smart_scraper/add_metadata.rb
@@ -6,9 +6,10 @@ require_relative Info.paths["ruby_tools"] # the (path) inside info.yaml
 all_urls = JSON.load(FS.read($paths["all_urls"]))
 
 # find all the not-already-checked videos
-unchecked_keys = all_urls.keys.select do |each|
-    all_urls[each].is_a?(Hash) && all_urls[each]["duration"] == nil && ! all_urls[each]["unavailable"]
-end.dup
+unchecked_keys = all_urls.keys.dup
+# .select do |each|
+#     all_urls[each].is_a?(Hash) && all_urls[each]["duration"] == nil && ! all_urls[each]["unavailable"]
+# end.dup
 
 # 
 # TODO: make this process continuous, right now it just gets an initial snapshot of the data and then overwrites the file
@@ -25,11 +26,12 @@ save_metadata = ->() do
                 url = get_full_url(video)
                 # get the new metadata
                 metadata = get_metadata_for(url)
+                puts "metadata is: #{metadata} "
                 # update the metadata for that video
                 all_urls[video].merge!(metadata)
             rescue => exception
                 exception_string = "#{exception}".strip
-                exception_string.gsub!(/765: unexpected token at '([\w\W]*)'/, '\1')
+                exception_string.gsub!(/765: unexpected token at '([\w\W]*)'/, '\1')&.strip!
                 if exception_string =~ /ERROR: \w+: YouTube said: This video is not available./
                     puts "    #{video}:  YouTube said: video is not available".yellow
                     all_urls[video] = false
@@ -39,10 +41,12 @@ save_metadata = ->() do
                 elsif exception_string =~ /ERROR: \w+: YouTube said: This video has been removed by the uploader/
                     puts "    #{video}:  removed by uploader".yellow
                     all_urls[video] = false
-                elsif metadata.keys.size == 0
+                else
                     # if there was an error in the thread, just let the thread close. Dont stop the whole program
                     puts "    there was an error on one of the videos, #{video}:  #{exception_string}".yellow
-                    all_urls[video] = nil
+                    if metadata.keys.size == 0
+                        all_urls[video] = nil
+                    end
                 end
             end
         end
@@ -54,6 +58,18 @@ end
 # 
 # create some threads for grabbing metdata
 threads = []
+
+# create a thread for occasionally saving data to a file
+threads.push Thread.new {
+    loop do
+        puts "Saving to disk. #{unchecked_keys.size()} urls remaining".blue
+        # wait a bit before writing to disk
+        sleep PARAMETERS["add_metadata"]["save_to_file_frequency"]
+        # overwrite the file
+        FS.write(all_urls.to_json, to: $paths["all_urls"])
+    end
+}
+
 # create the metadata gatherers
 for each in 1..PARAMETERS["add_metadata"]["number_of_threads"]
     thread_number = each.to_i
@@ -67,16 +83,6 @@ for each in 1..PARAMETERS["add_metadata"]["number_of_threads"]
     }
 end
 
-# create a thread for occasionally saving data to a file
-threads.push Thread.new {
-    loop do
-        puts "Saving to disk. #{unchecked_keys.size()} urls remaining".blue
-        # wait a bit before writing to disk
-        sleep PARAMETERS["add_metadata"]["save_to_file_frequency"]
-        # overwrite the file
-        FS.write(all_urls.to_json, to: $paths["all_urls"])
-    end
-}
 
 # wait for all of the threads to finish
 threads.map{ |each| each.join() }
