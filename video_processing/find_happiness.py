9062cc85fe6bb75942444c726ad6b47e865b9a51
jeff-hykin <jeff.hykin@gmail.com>
2020-06-03 19:09:07 -0500

    - add outline for finding happiness

---
+++ video_processing/find_happiness.py
@@ -0,0 +1,13 @@
+
+# select videos
+    # have a selector tool
+    # - can prefer videos that are already cached
+    # - can select videos randomly
+    # - can place filter requirements (e.g. duration, quality, etc)
+# once video is selected have it auto-download if needed
+    # have a default cache that is machine specific and checked based on video id
+    # store the cache inside the database by default
+# once the video is ready, select which frames to process
+# then, on each frame, run the existing emotion recognition code
+# call the video_database tool, use the video_id to add additional infomation to the video
+# done
