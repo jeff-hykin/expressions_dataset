5abc0e8ad93a03107ea23693f1861ad0e61ed89f
jeff-hykin <jeff.hykin@gmail.com>
2020-06-09 14:52:47 -0500

    add test script

--- video_processing/find_happiness.py
+++ video_processing/find_happiness.py
@@ -3,19 +3,23 @@ from os.path import join, dirname
 exec(Path(join(dirname(__file__), '..', 'toolbox', 'tools.py')).read_text())
 from toolbox.face_tools.expressions.Facial_expressions_detection import label as get_face_emotions
 
-image = Image("database/eyebrow_raise_data_refined/vid_1/50.png")
-
-print(json.dumps(get_face_emotions(image.img)))
-image.bounding_boxes().save(to="./test_img")
-
 # grab some videos
-# for each_video in VideoSelect().is_downloaded.then.has_basic_info.has_related_videos.retrive():
-# # videos shorter than 5 minutes
-# if each_video["basic_info"]["duration"] < (5 * 60):
-#     new_frame_data = {}
-#     # label get all the frames 
-#     for each_index, each_frame in enumerate(each_video.frames()):
-#         # save the data on a per-frame basis
-#         new_frame_data[each_index] = { "emotion_0.0.1" : get_face_emotions(each_frame) }
-#     # send the updated information to the database
-#     each_video.merge_data({"frames": new_frame_data })
+for each_video in VideoSelect().is_downloaded.then.has_basic_info.has_related_videos.retrive():
+    print('each_video.data = ', each_video.data)
+    # videos shorter than 5 minutes
+    if each_video["basic_info"]["duration"] < (5 * 60):
+        new_frame_data = {}
+        # label get all the frames 
+        for each_index, each_frame in enumerate(each_video.frames):
+            print('each_index = ', each_index)
+            # save the data on a per-frame basis
+            new_frame_data[each_index] = { "emotion_0.0.1" : get_face_emotions(each_frame) }
+            # FIXME: Debugging 
+            print('new_frame_data = ', new_frame_data)
+            Image(each_frame).save(to="./test_image")
+            print('saved image')
+            exit()
+
+        # send the updated information to the database
+        # FIXME: Debugging 
+        # each_video.merge_data({"frames": new_frame_data })
