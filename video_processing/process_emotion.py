cb4387df3b42123d7494f23959f4ce036a5f9c68
jeff-hykin <jeff.hykin@gmail.com>
2020-06-12 12:48:06 -0500

    add stats and better logging to emotion_process

--- video_processing/process_emotion.py
+++ video_processing/process_emotion.py
@@ -11,19 +11,50 @@ def get_faces(image):
     face_dimensions = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=1, minSize=(100, 100), flags=cv.CASCADE_SCALE_IMAGE)
     cropped_faces = [ image[y:y + h, x:x + w] for x, y, w, h in face_dimensions ]
     return cropped_faces, face_dimensions
-    
+
+# 
+# performance statistics
+# 
+stats = {
+    "video_frame_counts": [],
+    "video_processing_times": [],
+    "global_start_time": time.time(),
+    "global_duration": 0,
+    "number_of_successful_videos" : 0,
+    "number_of_videos_attempted" : 0,
+    "local_start_time": 0,
+}
+
 # grab some videos
+stop_on_next_video = 0
 for video_count, each_video in enumerate(VideoSelect().is_downloaded.then.has_basic_info.has_related_videos.retrive()):
+    
+    # logging
+    stats["number_of_videos_attempted"] += 1
+    stats["global_duration"] = time.time() - stats["global_start_time"]
     print(f"[   video={video_count}] {each_video.id}")
+    
+    # stop from keyboard
+    if stop_on_next_video > 0:
+        print('process_emotion successfully shutdown gracefully')
+        break
+    
     # videos shorter than 5 minutes
     if each_video["basic_info"]["duration"] < (5 * 60):
         try:
+            stats["local_start_time"] = time.time()
+            
+            # each video frame
             for each_index, each_frame in enumerate(each_video.frames):
+                
+                # logging
                 if each_index % 325 == 0:
                     print(f"\n[   frame={each_index}]",end="")
                 elif each_index % 25 == 0:
                     print(f"[frame={each_index}]",end="")
                     sys.stdout.flush()
+                
+                # actual machine learning usage
                 face_images, dimensions = get_faces(each_frame)
                 face_data = []
                 for each_face_img, each_dimension in zip(face_images, dimensions):
@@ -34,15 +65,27 @@ for video_count, each_video in enumerate(VideoSelect().is_downloaded.then.has_ba
                         "height" : int(each_dimension[3]),
                         "emotion_vgg19_0-0-2" : get_emotion_data(preprocess_face(each_face_img)),
                     })
-                # save the results of the frame to the database
+                
+                # saves each frame info to database
                 each_video["frames", each_index, "faces_haarcascade_0-0-2"] = face_data
+            # 
+            # stats
+            # 
+            stats["number_of_successful_videos"] += 1
+            stats["video_frame_counts"].append(each_index)
+            stats["video_processing_times"].append( time.time() - stats["local_start_time"] )
+            FS.write(json.dumps(stats), to="./process_emotion.stats.nosync.json")
+        
         except KeyboardInterrupt:
-            exit(0)
-        # skip videos that can't be downloaded
+            print('\nGot the message, stopping on video completion\nkeep interrupting to force cancel midway')
+            stop_on_next_video += 1
+            # if the user says to end repeatedly
+            if stop_on_next_video > 5:
+                # stop immediately, causing partially bad data
+                exit(0)
+        
+        # skip videos that can't be downloaded/processed for whatever reason
         except Exception as the_exception:
             print(f"[video select] {each_video.id} hit an error while in processing_emotion...skipping")
             print(f'[exception] {the_exception}')
             continue
-
-        # send the updated information to the database
-        # each_video.merge_data({"frames": new_frame_data })
