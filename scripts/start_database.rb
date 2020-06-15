2c30c74b0088be3a69ed5ff8121cd26c8c465773
jeff-hykin <jeff.hykin@gmail.com>
2020-06-15 13:14:58 -0500

    add better windows support

--- scripts/start_database.rb
+++ scripts/start_database.rb
@@ -3,12 +3,31 @@ require_relative Info.paths["ruby_tools"] # corrisponds to the "(path)" inside i
 
 name_of_docker_database_container = "database"
 docker_image = LocalDocker.new(name_of_docker_database_container)
-docker_image.run(
-    arguments: [],
-    options:[
-        :interactive,
-        :remove_after_completion,
-        :access_to_current_enviornment,
-        setup_port = "--publish #{PARAMETERS["database"]["port"]}:3000 --publish 27017:27017",
-    ]
-)
+
+# perform a kind of work-around for windows
+if OS.is?(:windows)
+    system(
+        "docker",
+        "run",
+        "-it",  # interactive
+        "--rm", # remove it after its done
+        # which port
+        *["--publish", "#{PARAMETERS["database"]["port"]}:3000" ],
+        # mount the database files
+        *["--volume",   $paths['database_data'] + ":/data/db" ],
+        *["--volume",   $paths['database_server'] + ":/project" ],
+        # which image to use
+        docker_image.image_name
+    )
+else
+    # use the normal tools for other OS's
+    docker_image.run(
+        arguments: [],
+        options:[
+            :interactive,
+            :remove_after_completion,
+            :access_to_current_enviornment,
+            setup_port = "--publish #{PARAMETERS["database"]["port"]}:3000 --publish 27017:27017",
+        ]
+    )
+end
