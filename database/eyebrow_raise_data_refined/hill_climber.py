c19547e576a1d0a993779f0a7f3875d293b59948
Hykin <jeff.hykin@tamu.edu>
2020-06-22 17:12:06 -0500

    Revert "stop tracking database files"
    This reverts commit c3875a2eb926a21e04a8ada7befd9a61e77e03b9.

---
+++ database/eyebrow_raise_data_refined/hill_climber.py
@@ -0,0 +1,85 @@
+
+# for each parameter, have a scaling amount, and a default value
+    # generate a random-normal value for each parameter
+    # follow the best solution
+
+
+
+# TODO: improvements
+    # use naieve bayes to classify how much a certain parameter contributes to the fitness
+    # use acceleration (x2) on consistent aspects, use (/2) on nearly-random aspects
+
+
+from numpy.random import choice
+import numpy as np
+              
+def king(parameters, population_growth, fitness_function):
+    """
+    parameters: [ (inital_value, parameter_scale), (inital_value, parameter_scale) ]
+        a list of tuples, each one corrisponds to an argument in the fitness_function (below)
+    population_growth:
+        how many people are added to the king's kingdom
+        usually keep this number above 4, but limit it to ~20x the thread count on your CPU
+    fitness_function:
+        a function
+            - accepts some number of numeric arguments
+            - every argument must handle negative and floating point values
+                - this is necessary even if the fitness function just takes the floor/ceiling of a value
+                  or just makes all negative values 0
+            - returns a numeric value where a higher number indicates a better score
+    """
+    standard_deviations = [ each for _, each in parameters ]
+    king_parameters = [ each for each, _ in parameters ]
+    king_fitness = fitness_function(*king_parameters)
+    king = (king_fitness, king_parameters)
+    people = [ king ]
+    
+    while True:
+        # 
+        # pick a king
+        # 
+        all_fitness = sum([ fitness for fitness, parameters in people ])
+        probability_of_being_king = [ fitness/all_fitness for fitness, parameters in people ]
+        # randomly select the king based on fitness
+        king = people[choice(list(range(len(people))), 1, p=probability_of_being_king)[0]]
+        
+        
+        
+        # remove the bottom 1/3 of the population
+        if len(people) > 3:
+            people = sorted(people, key=lambda x: x[0])
+            number_of_people_to_remove = int(len(people)/3.0)
+            people = people[ number_of_people_to_remove: ]
+        
+        # generate a new population based on the king
+        king_parameters = king[1]
+        for each in range(population_growth):
+            # generate features
+            person_parameters = []
+            for each_index, each in enumerate(king_parameters):
+                person_parameters.append(np.random.normal(loc=each, scale=standard_deviations[each_index]))
+            
+            # TODO: could probably run fitness in parallel 
+            person_fitness = fitness_function(*person_parameters)
+            people.append((person_fitness, person_parameters))
+        
+        # every iteration results in a generation of outcomes
+        yield people
+
+
+
+# example usage
+
+# def fitness(arg1, arg2):
+#     return arg1 * arg2
+
+# for each_iteration, each in enumerate(king([(100,10), (100, 10)], 20, fitness)):
+#     if each_iteration > 20:
+#         break
+    
+#     people = sorted(each, key=lambda x: x[0])
+#     # print results
+#     print(f"generation: {each_iteration}")
+#     for each_person in people:
+#         print(f"    {each_person}")
+    
---
+++ database/eyebrow_raise_data_refined/validation.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from os.path import join, dirname
+import matplotlib.pyplot as plt
+exec(Path(join(dirname(__file__),'..', '..', 'toolbox', 'tools.py')).read_text())
+
+
+def scale_0_to_100(a_list):
+    min_score = min(a_list)
+    shifted_scores = [ each - min_score for each in a_list ]
+    max_score = max(shifted_scores)
+    scale_to_100 = 100.0 / max_score
+    adjusted_scores = [ each * scale_to_100 for each in shifted_scores ]
+    return adjusted_scores
+
+# create a figure for showing performance
+
+here = dirname(__file__)
+
+for video_index, each_video_path in enumerate(FS.list_files(paths["raised_eyebrows_videos"])):
+    # 
+    # get the JSON data for each video
+    # 
+    *folders, video_filename, extension = FS.path_pieces(each_video_path)
+    each_video = Video(each_video_path)
+    json_file_path = FS.join(here, video_filename, 'info.json')
+    all_frame_data = {}
+    # if the json already exists, pull in the data
+    if FS.exists(json_file_path):
+        with open(json_file_path) as json_file:
+            all_frame_data = json.load(json_file)
+        
+        frames = list(all_frame_data.values())
+        
+        # if there is hand-picked data for the video
+        if all( each.get("handpicked_eyebrow_score", None) != None for each in frames):
+            # 
+            # plot the data
+            # 
+            # create a new graph for each video
+            plt.figure(video_index)
+            # graph each aspect
+            for each_term in [ "handpicked_eyebrow_score", "mouth_openness" ]:
+                plt.plot( [ each_frame[each_term] for each_frame in frames ], label=each_term )
+            
+            # scale the eyebrow score 
+            plt.plot(
+                scale_0_to_100([ each_frame["eyebrow_raise_score"] for each_frame in frames ]),
+                label="eyebrow_raise_score"
+            )
+            plt.legend(loc='best')
+
+# show all the videos
+plt.show()
