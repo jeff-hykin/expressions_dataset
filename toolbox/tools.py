380e98c15703dc1518878d86a679be72e5fbdb21
jeff-hykin <jeff.hykin@gmail.com>
2020-06-25 17:44:55 -0500

    add some commandline tools

--- toolbox/tools.py
+++ toolbox/tools.py
@@ -24,6 +24,66 @@ import yaml
 PROJECT_ROOT = dirname(dirname(__file__))
 sys.path.append(PROJECT_ROOT)
  
+def progress(percent=0, width=30):
+    left = width * percent // 100
+    right = width - left
+    print(
+        '\r[', '#' * left, ' ' * right, ']',
+        f' {percent:.0f}%',
+        sep='',
+        end='',
+        flush=True
+    )
+
+# also, a wrapper function/vars for adding some color
+foreground_colors = {  
+    "black"          : 30,
+    "red"            : 31,
+    "green"          : 32,
+    "yellow"         : 33,
+    "blue"           : 34,
+    "magenta"        : 35,
+    "cyan"           : 36,
+    "white"          : 37,
+    "bright_black"   : 90,
+    "bright_red"     : 91,
+    "bright_green"   : 92,
+    "bright_yellow"  : 93,
+    "bright_blue"    : 94,
+    "bright_magenta" : 95,
+    "bright_cyan"    : 96,
+    "bright_white"   : 97,
+}
+
+background_colors = {
+    "black"          : 40,
+    "red"            : 41,
+    "green"          : 42,
+    "yellow"         : 43,
+    "blue"           : 44,
+    "magenta"        : 45,
+    "cyan"           : 46,
+    "white"          : 47,
+    "bright_black"   : 100,
+    "bright_red"     : 101,
+    "bright_green"   : 102,
+    "bright_yellow"  : 103,
+    "bright_blue"    : 104,
+    "bright_magenta" : 105,
+    "bright_cyan"    : 106,
+    "bright_white"   : 107,
+}
+
+def color(string, foreground="white", background="black"):
+    if foreground not in foreground_colors:
+        raise Exception(f"couldn't find foreground color {foreground}")
+    if background not in background_colors:
+        raise Exception(f"couldn't find background_color color {background}")
+    foreground_number = foreground_colors[foreground]
+    background_number = background_colors[background]
+    return f'\033[{foreground_number}m\033[{background_number}m{string}\033[0m'
+
+
 import os
 import glob
 import shutil
