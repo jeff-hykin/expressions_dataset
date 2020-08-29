968aa5829e65c75f47412c9c6ead589710f1468f
jeff-hykin <jeff.hykin@gmail.com>
2020-08-29 16:29:07 -0500

    - add cors

--- database_management/express_server/main_code/server.js
+++ database_management/express_server/main_code/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const parameters = require('../package.json').parameters
-
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 app.use(bodyParser.json({limit: '50mb', extended: true}))
 
 module.exports = {
