2a3e7663c4e134ba6ead93764012b2ccbe7c5391
jeff-hykin <jeff.hykin@gmail.com>
2020-09-21 13:20:31 -0500

    -

--- database_management/express_server/start_database.sh
+++ database_management/express_server/start_database.sh
@@ -3,6 +3,6 @@ sh ./actions/start_mongo.sh &
 # install all the needed libs for the express server
 npm install
 # keep the express server running and tell it to watch for updates
-nodemon main.js &
+node main.js &
 # allow the user to control the system
 bash
