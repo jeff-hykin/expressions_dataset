7805b2073435910ff8d920a963b6634b2a91a1b9
jeff-hykin <jeff.hykin@gmail.com>
2020-10-07 12:38:29 -0500

    -

--- database_management/express_server/main_code/endpoints.js
+++ database_management/express_server/main_code/endpoints.js
@@ -68,9 +68,9 @@ module.exports = {
         // this returns the uuid of the newly created moment
         endpointWithReturnValue(`addSegmentObservation`, async ([observationEntry]) => {
             // basic checks on the input
-            requireThat({value: observationEntry.videoId,     is: String, failMessage: `\`videoId\` should be the id (string) of for the video. Instead it was ${videoId}`})
-            requireThat({value: observationEntry.startTime,   is: Number, failMessage: `The \`startTime\` should be an float (seconds). Instead it was ${startTime}`      })
-            requireThat({value: observationEntry.endTime,     is: Number, failMessage: `The \`endTime\` should be an float (seconds). Instead it was ${endTime}`          })
+            requireThat({value: observationEntry.videoId,     is: String, failMessage: `\`videoId\` should be the id (string) of for the video. Instead it was ${observationEntry.videoId}`})
+            requireThat({value: observationEntry.startTime,   is: Number, failMessage: `The \`startTime\` should be an float (seconds). Instead it was ${observationEntry.startTime}`      })
+            requireThat({value: observationEntry.endTime,     is: Number, failMessage: `The \`endTime\` should be an float (seconds). Instead it was ${observationEntry.endTime}`          })
             requireThat({value: observationEntry.observer,    is: String, failMessage: `\`observer\` should be a unique id for what process/human created the data. Instead it was ${observationEntry.observer}`})
             requireThat({value: observationEntry.observation, is: Object, failMessage: `\`observation\` should be an object/dictionary. Instead it was ${observationEntry.observation}`})
             
