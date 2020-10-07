b1e96dc00b92459f6d9c7adb196f810905221d41
jeff-hykin <jeff.hykin@gmail.com>
2020-10-07 12:57:43 -0500

    -

--- database_management/express_server/actions/debug_setup.js
+++ database_management/express_server/actions/debug_setup.js
@@ -11,7 +11,7 @@ Object.defineProperty(Object.prototype, "wait", {
 })
 
 global.endpoints = require('require-all')({
-    dirname:  __dirname + '/../endpoints',
+    dirname:  __dirname + '/../main_code/endpoints',
     filter:  /.+\.js$/,
     recursive: true
 })
