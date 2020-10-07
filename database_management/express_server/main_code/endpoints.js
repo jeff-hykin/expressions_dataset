b5748c605711a1245856e3ba98a32bd22e67a9b3
jeff-hykin <jeff.hykin@gmail.com>
2020-10-07 13:09:58 -0500

    -

--- database_management/express_server/main_code/endpoints.js
+++ database_management/express_server/main_code/endpoints.js
@@ -60,7 +60,6 @@ module.exports = {
         
         // setup all the file-based endpoints
         global.endpoints = endpoints
-        console.debug(`endpoints is:`,endpoints)
         for (let eachName in endpoints) {
             endpointWithReturnValue(eachName, endpoints[eachName])
         }
@@ -75,31 +74,6 @@ module.exports = {
             })
         }
         
-        // this returns the uuid of the newly created moment
-        endpointWithReturnValue(`addSegmentObservation`, async ([observationEntry]) => {
-            console.debug(`observationEntry is:`,observationEntry)
-            // basic checks on the input
-            requireThat({value: observationEntry.videoId,     is: String, failMessage: `\`videoId\` should be the id (string) of for the video. Instead it was ${observationEntry.videoId}`})
-            requireThat({value: observationEntry.startTime,   is: Number, failMessage: `The \`startTime\` should be an float (seconds). Instead it was ${observationEntry.startTime}`      })
-            requireThat({value: observationEntry.endTime,     is: Number, failMessage: `The \`endTime\` should be an float (seconds). Instead it was ${observationEntry.endTime}`          })
-            requireThat({value: observationEntry.observer,    is: String, failMessage: `\`observer\` should be a unique id for what process/human created the data. Instead it was ${observationEntry.observer}`})
-            requireThat({value: observationEntry.observation, is: Object, failMessage: `\`observation\` should be an object/dictionary. Instead it was ${observationEntry.observation}`})
-            
-            let idForNewMoment = generateUuid()
-            console.debug(`idForNewMoment is:`,idForNewMoment)
-
-            // set the new moment
-            await collectionMethods.set({
-                keyList: [ idForNewMoment ],
-                from: "observations",
-                to: {
-                    ...observationEntry,
-                    type: "segment",
-                },
-            })
-            return idForNewMoment
-        })
-
         // 
         // just a ping method to check if running/accessible
         // 
