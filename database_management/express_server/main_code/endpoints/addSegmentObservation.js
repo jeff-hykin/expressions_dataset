c5fea87177b2784e779f577a665d2ed5e18ddba9
Jeff Hykin <jeff.hykin@gmail.com>
2020-10-11 19:39:16 -0400

    - addMultipleSegments

--- database_management/express_server/main_code/endpoints/addSegmentObservation.js
+++ database_management/express_server/main_code/endpoints/addSegmentObservation.js
@@ -1,15 +1,15 @@
 const { recursivelyAllAttributesOf, get, merge, valueIs, logBlock, dynamicSort, checkIf, requireThat } = require("good-js")
 const { v4: generateUuid } = require('uuid')
 const { smartEndpoints, collectionMethods, } = require("../endpoint_tools")
+const validateObservation = require("../../structures/validate_observation")
 
 module.exports = async ([observationEntry]) => {
     console.debug(`observationEntry is:`,observationEntry)
     // basic checks on the input
-    requireThat({value: observationEntry.videoId,     is: String, failMessage: `\`videoId\` should be the id (string) of for the video. Instead it was ${observationEntry.videoId}`})
-    requireThat({value: observationEntry.startTime,   is: Number, failMessage: `The \`startTime\` should be an float (seconds). Instead it was ${observationEntry.startTime}`      })
-    requireThat({value: observationEntry.endTime,     is: Number, failMessage: `The \`endTime\` should be an float (seconds). Instead it was ${observationEntry.endTime}`          })
-    requireThat({value: observationEntry.observer,    is: String, failMessage: `\`observer\` should be a unique id for what process/human created the data. Instead it was ${observationEntry.observer}`})
-    requireThat({value: observationEntry.observation, is: Object, failMessage: `\`observation\` should be an object/dictionary. Instead it was ${observationEntry.observation}`})
+    let result = validateObservation(observationEntry)
+    if (result !== true) {
+        throw Error(result)
+    }
     
     let idForNewMoment = generateUuid()
     
---
+++ database_management/express_server/structures/validate_observation.js
@@ -0,0 +1,18 @@
+const { recursivelyAllAttributesOf, get, merge, valueIs, logBlock, dynamicSort, checkIf, requireThat } = require("good-js")
+/**
+ * Function
+ *
+ * @param {Object} observation
+ * @return {Boolean|String} true if valid, string-error if invalid
+ *
+ */
+module.exports = (observation)=>{
+    if (observation.type == "segment") {
+        if (!checkIf({value: each.videoId,     is: String})) { return `\`videoId\` should be the id (string) of for the video. Instead it was ${each.videoId}` }
+        if (!checkIf({value: each.startTime,   is: Number})) { return `The \`startTime\` should be an float (seconds). Instead it was ${each.startTime}`       }
+        if (!checkIf({value: each.endTime,     is: Number})) { return `The \`endTime\` should be an float (seconds). Instead it was ${each.endTime}`           }
+        if (!checkIf({value: each.observer,    is: String})) { return `\`observer\` should be a unique id for what process/human created the data. Instead it was ${each.observer}` }
+        if (!checkIf({value: each.observation, is: Object})) { return `\`observation\` should be an object/dictionary. Instead it was ${each.observation}` }
+    }
+    return true
+}
