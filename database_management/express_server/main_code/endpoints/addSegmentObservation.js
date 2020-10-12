fe3aba9041ee1ae5262e5119ff2cc6bce2445a1c
Jeff Hykin <jeff.hykin@gmail.com>
2020-10-11 21:33:14 -0400

    -

--- database_management/express_server/main_code/endpoints/addSegmentObservation.js
+++ database_management/express_server/main_code/endpoints/addSegmentObservation.js
@@ -3,27 +3,28 @@ const { v4: generateUuid } = require('uuid')
 const { smartEndpoints, collectionMethods, } = require("../endpoint_tools")
 const validateObservation = require("../../structures/validate_observation")
 
-module.exports = async ([observationEntry]) => {
-    console.debug(`observationEntry is:`,observationEntry)
-    // basic checks on the input
-    // let result = validateObservation(observationEntry)
-    // if (result !== true) {
-    //     throw Error(result)
-    // }
-    // console.log('hodwy 1')
-    let idForNewMoment = generateUuid()
-    // console.log('hodwy 2')
+module.exports = async () => {
+    // [observationEntry]
+    // console.debug(`observationEntry is:`,observationEntry)
+    // // basic checks on the input
+    // // let result = validateObservation(observationEntry)
+    // // if (result !== true) {
+    // //     throw Error(result)
+    // // }
+    // // console.log('hodwy 1')
+    // let idForNewMoment = generateUuid()
+    // // console.log('hodwy 2')
     
-    // // set the new moment
-    // await collectionMethods.set({
-    //     keyList: [ idForNewMoment ],
-    //     from: "observations",
-    //     to: {
-    //         ...observationEntry,
-    //         type: "segment",
-    //     },
-    // })
-    // console.log('hodwy 3')
+    // // // set the new moment
+    // // await collectionMethods.set({
+    // //     keyList: [ idForNewMoment ],
+    // //     from: "observations",
+    // //     to: {
+    // //         ...observationEntry,
+    // //         type: "segment",
+    // //     },
+    // // })
+    // // console.log('hodwy 3')
     
-    return idForNewMoment
+    // return idForNewMoment
 }
