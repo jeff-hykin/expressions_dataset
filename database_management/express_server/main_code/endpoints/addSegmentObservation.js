2a7c6689327935484c9ca95e09a9380dca3cba0f
Jeff Hykin <jeff.hykin@gmail.com>
2020-10-11 21:27:27 -0400

    -

--- database_management/express_server/main_code/endpoints/addSegmentObservation.js
+++ database_management/express_server/main_code/endpoints/addSegmentObservation.js
@@ -5,13 +5,15 @@ const validateObservation = require("../../structures/validate_observation")
 
 module.exports = async ([observationEntry]) => {
     console.debug(`observationEntry is:`,observationEntry)
+    console.debug(`validateObservation is:`,validateObservation)
     // basic checks on the input
     let result = validateObservation(observationEntry)
     if (result !== true) {
         throw Error(result)
     }
-    
+    print('hodwy 1')
     let idForNewMoment = generateUuid()
+    print('hodwy 2')
     
     // set the new moment
     await collectionMethods.set({
@@ -22,6 +24,7 @@ module.exports = async ([observationEntry]) => {
             type: "segment",
         },
     })
+    print('hodwy 3')
     
     return idForNewMoment
 }
